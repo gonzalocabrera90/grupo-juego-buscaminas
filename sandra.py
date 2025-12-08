bce9e68735fe779e507616b7dd8d1da7de4085cb
Sandra <estudiodivasson@gmail.com>
2025-12-08 19:32:52 -0300

    Cargue función niveles

--- sandra.py
+++ sandra.py
@@ -3,3 +3,20 @@ def sandra():
 
 if __name__ == "__main__":
 	sandra()
+
+def niveles():
+    print("--------NIVELES--------")
+    print("ELIGE UNA OPCIÓN: ")
+    print("1. PRINCIPIANTE.")
+    print("2. INTERMEDIO.")
+    print("3. EXPERTO.")
+    print("4. PERSONALIZADO.")
+    #elige la opción
+    opcion= input("")
+    while True:
+        if opcion in ["1","2","3","4"]:
+            crear_tableros(opcion)
+            break
+        else:
+            print("OPCION INCORRECTA, ELIGE DE NUEVO.")
+            niveles()
