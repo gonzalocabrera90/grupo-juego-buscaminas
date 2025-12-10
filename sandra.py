8512d711ef0ef57ea32036b7fbd2926b6f9c4cc9
Sandra <estudiodivasson@gmail.com>
2025-12-10 19:54:13 -0300

    Modifique nivel personalizado agregando el while y el if anidado.

--- sandra.py
+++ sandra.py
@@ -44,12 +44,20 @@ def crear_tableros(nivel):
         tablero2 = gonzalo.generar_matriz_minas(10, 10, 20)
         lugares_a_descubrir = 10*10 - 20
     else:
-        filas = int(input("ingrese la cantidad de filas: "))
-        columnas = int(input("ingrese la cantidad de columnas: "))
-        minas = int(input("ingrese la cantidad de minas: "))
-        tablero1 = gonzalo.generar_matriz_jugador(filas, columnas)
-        tablero2 = gonzalo.generar_matriz_minas(filas, columnas, minas)
-        lugares_a_descubrir = filas*columnas - minas
+        while True:
+            filas = int(input("ingrese la cantidad de filas: "))
+            columnas = int(input("ingrese la cantidad de columnas: "))
+            minas = int(input("ingrese la cantidad de minas: "))
+            if filas >2 and columnas >2:
+                if 2 < minas < (filas*columnas):
+                    tablero1 = gonzalo.generar_matriz_jugador(filas, columnas)
+                    tablero2 = gonzalo.generar_matriz_minas(filas, columnas, minas)
+                    lugares_a_descubrir = filas*columnas - minas
+                    break
+                else:
+                    print("La cantidad de minas ingresada no corresponde a la cantidad de lugares. Ingrese nuevamente.")
+            else:
+                print("La cantidad de filas y columnas debe ser mayor a 2. Ingrese nuevamente.")
     return tablero1, tablero2, lugares_a_descubrir
 
 def ingresar_coordenadas():
